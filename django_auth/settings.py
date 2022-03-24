006728c4f74ec3ae40461687c8748335ca02e22b
rylequi <119874112+rylequi@users.noreply.github.com>
2022-03-24 11:49:40 +0000

    Header update

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -21,3 +21,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6+@wfiy6uxgap1c)+^lk^02a=!yud0t46!npca+tkk$+jgx7_m'
