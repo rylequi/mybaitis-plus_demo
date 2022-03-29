e135485ab70c593fa2588bd43f328cb851f359b1
rylequi <119874112+rylequi@users.noreply.github.com>
2022-03-29 13:24:08 +0000

    Add Existing Files

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -36,3 +36,5 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
