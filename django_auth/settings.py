953abab9c89e9b998a29a0f4a3f9e5b0ba4ceed9
rylequi <119874112+rylequi@users.noreply.github.com>
2022-04-06 12:25:02 +0000

    update .po files

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -50,3 +50,4 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
