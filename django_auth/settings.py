a81adb9b5984a06006db9daa64b7b2ba622ce8fb
rylequi <119874112+rylequi@users.noreply.github.com>
2022-04-09 09:35:30 +0000

    release 1.6.1

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -57,3 +57,5 @@ ROOT_URLCONF = 'django_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
