aebd3f40cae2bfd91e1a64ae81fbf803cb4bab27
rylequi <119874112+rylequi@users.noreply.github.com>
2022-04-17 18:04:51 +0000

    Fix some quality flaws

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -81,3 +81,4 @@ WSGI_APPLICATION = 'django_auth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
