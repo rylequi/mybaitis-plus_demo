e21bbd6c210f6ae5352fa849b8e329836572dba3
rylequi <119874112+rylequi@users.noreply.github.com>
2022-05-07 20:02:38 +0000

    Update slip.blade.php

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -131,3 +131,4 @@ LOGIN_REDIRECT_URL = 'home'
 AUTHENTICATION_BACKENDS = (
  'social_core.backends.open_id.OpenIdAuth',
  'social_core.backends.google.GoogleOpenId',
+ 'social_core.backends.google.GoogleOAuth2',
