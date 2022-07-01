7e327153635c3dcbf7ba44ac4e1057b524f2b986
rylequi <119874112+rylequi@users.noreply.github.com>
2022-07-01 19:15:06 +0000

    Fix some broken links

--- django_auth/wsgi.py
+++ django_auth/wsgi.py
@@ -1,4 +1,4 @@
 """
 WSGI config for django_auth project.
 
-It exposes the WSGI callable as a module-level variable named ``application`
+It exposes the WSGI callable as a module-level variable named ``application``.
