6a664e705cc3ed05029fb6e589cd1adf7ddcd7b2
rylequi <119874112+rylequi@users.noreply.github.com>
2022-08-10 08:02:56 +0000

    Bump 0.0.2

--- django_auth/wsgi.py
+++ django_auth/wsgi.py
@@ -4,4 +4,4 @@ WSGI config for django_auth project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.d
+https://docs.djan
