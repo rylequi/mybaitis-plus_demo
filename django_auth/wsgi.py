57c2e86c7ba6a786890862da91d20cbd9e88d9e3
rylequi <119874112+rylequi@users.noreply.github.com>
2022-09-03 06:27:01 +0000

    [MERGE] Sync with trunk

--- django_auth/wsgi.py
+++ django_auth/wsgi.py
@@ -4,4 +4,4 @@ WSGI config for django_auth project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/
+https://docs.djangoproject.com/en/1.11/howto/deployment/w
