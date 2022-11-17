779a04dafe20bb8d43146f9b16a13e003dcf7fb8
rylequi <119874112+rylequi@users.noreply.github.com>
2022-11-17 13:24:29 +0000

    Readability.

--- django_auth/wsgi.py
+++ django_auth/wsgi.py
@@ -11,4 +11,4 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_auth.sett
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_auth.setti
