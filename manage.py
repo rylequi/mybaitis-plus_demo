10114b852e701ab9e8b2dc42606cdfbbdda3242e
rylequi <119874112+rylequi@users.noreply.github.com>
2022-12-26 11:32:48 +0000

    Fixing syntax

--- manage.py
+++ manage.py
@@ -6,3 +6,5 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_auth.settings")
     try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
