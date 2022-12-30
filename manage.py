4525d968b69bc4e81a1bdb4606084d59304b2bda
rylequi <119874112+rylequi@users.noreply.github.com>
2022-12-30 21:11:30 +0000

    Version 1.1r22

--- manage.py
+++ manage.py
@@ -18,3 +18,4 @@ if __name__ == "__main__":
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
+            )
