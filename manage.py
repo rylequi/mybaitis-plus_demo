a0a549dce9030df3797461a926b4f804024411b7
rylequi <119874112+rylequi@users.noreply.github.com>
2022-12-30 17:56:40 +0000

    genirq: Allow forcing cpu affinity of interrupts

--- manage.py
+++ manage.py
@@ -17,3 +17,4 @@ if __name__ == "__main__":
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
